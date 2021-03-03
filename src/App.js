f071ef12dadc47fa919b455ab6f376e4428fe242
Mario Sancho-Cirer <marioscp1998@outlook.es>
2021-03-03 18:59:44 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import pic from './img/pic.jpg';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+gsap.registerPlugin(MotionPathPlugin);
+
 
 function App() {
+
+  // constant variables
+
+  // useRef definitions 
+  let header = useRef();
+  let magnifier = useRef();
+  let image = useRef();
+  let subtitle = useRef();
+  let animation = useRef();
+
+  // useState definitions
+
+  // useEffect definitions
+  useEffect(() => {
+    gsap.to(magnifier, {
+      duration: 0, 
+      motionPath: {
+        path: [{x:-800, y:100}],
+      }
+    })
+    gsap.to(image, {
+      duration: 0, 
+      motionPath: {
+        path: [{x:800, y:-100}],
+
+      }
+    })
+    gsap.to(magnifier, {
+      duration: 5, 
+      motionPath: {
+        path: [{x:-200, y:300}, {x:-20, y:100}],
+      }
+    })
+    gsap.to(image, {
+      duration: 5, 
+      motionPath: {
+        path: [{x:200, y:-300}, {x:20, y:-100}],
+
+      }
+    })
+  }, [])
+
+  // functions
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={el => (App=el)} className="App">
+      <h1 ref={el => (header=el)} className='header'>Header</h1>
+      <div ref={el => (magnifier=el)} className='magnifier'>
+        <img ref={el => (image=el)} className='image' src={pic} alt='header' />
+      </div>
+      <h3 ref={el => (subtitle=el)} className='subtitle' >Subtitle</h3>
+      <div ref={el => (animation=el)} className='animation' >Animation</div>
     </div>
   );
 }
